bdc0f6ad241447c19dabbad335b6702139ecc34c
kimgh06 <kkh061101@naver.com>
2024-05-02 10:03:57 +0900

    edit scene to card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,40 @@ export default function Home() {
     <Canvas>
       <Suspense>
         {/* <Scene url='./textures/magic_card.jpg' position={[1.5, 0, 0]} /> */}
-        <Scene url='./textures/ac_right_hand.png' position={[-1, 0, 0]} />
-        <Scene url='./textures/ac_body.png' position={[0, 0, 0]} />
-        <Scene url='./textures/ac_left_hand.png' position={[1, 0, 0]} />
-        <Scene url='./textures/ac_left_leg.png' position={[0.5, -1.618, 0]} />
-        <Scene url='./textures/ac_right_leg.png' position={[-0.5, -1.618, 0]} />
+        <Card url='./textures/ac_right_hand.png' position={[-1, -1, 0]} />
+        <Card url='./textures/ac_body.png' position={[0, -1, 0]} />
+        <Card url='./textures/ac_left_hand.png' position={[1, -1, 0]} />
+        <Card url='./textures/ac_left_leg.png' position={[0.5, -1, 2]} />
+        <Card url='./textures/ac_right_leg.png' position={[-0.5, -1, 2]} />
         <OrbitControls />
       </Suspense>
     </Canvas>
   );
 }
 
-function Scene({ url, position }: { url: string, position: Vector3 }) {
+function Card({ url, position }: { url: string, position: Vector3 }) {
   const ref = useRef<Mesh | null>(null);
   const [turning, setTurning] = useState<boolean>(false);
+  const [isFliped, setIsFliped] = useState<boolean>(true);
   useFrame((_, delta: number) => {
-    if (ref?.current?.rotateY !== undefined && turning) {
-      ref.current.rotation.y += (Math.PI + 0.7) / 60 / 2;
-      console.log(ref.current.rotation.y)
+    if (ref?.current?.rotateY === undefined || !turning) {
+      return;
+    }
+    if (!isFliped) {
+      if (ref.current.rotation.y % (Math.PI * 2) < Math.PI) {
+        ref.current.rotation.y += (Math.PI + 1) / 60;
+      }
+      if (ref.current.rotation.y % (Math.PI * 2) > Math.PI) {
+        ref.current.rotation.y = Math.PI;
+      }
+    }
+    if (isFliped) {
+      if (ref.current.rotation.y % (Math.PI * 2) >= Math.PI) {
+        ref.current.rotation.y += (Math.PI + 1) / 60;
+      }
+      if (ref.current.rotation.y % (Math.PI * 2) < Math.PI) {
+        ref.current.rotation.y = 0;
+      }
     }
   })
   useEffect(() => {
@@ -35,11 +51,9 @@ function Scene({ url, position }: { url: string, position: Vector3 }) {
       setTimeout(() => {
         setTurning(false)
       }, 1000);
-    } else {
-      if (ref?.current?.rotateY !== undefined) {
-        console.log(ref.current.rotation.y % (Math.PI * 2))
-        ref.current.rotation.y = ref.current.rotation.y % (Math.PI * 2) > Math.PI ? Math.PI : 0
-      }
+    }
+    if (ref.current) {
+      ref.current.rotation.x = -Math.PI / 2
     }
   }, [turning])
   return (
@@ -49,7 +63,11 @@ function Scene({ url, position }: { url: string, position: Vector3 }) {
         <directionalLight />
         <Image scale={[1, 1.618]} url={url} />
         <Box onClick={() => {
+          if (turning) {
+            return;
+          }
           setTurning(true);
+          setIsFliped((e: boolean) => !e)
         }} args={[1, 1.618, 0.001]} position={[0, 0, -0.001]} material-color={'gray'} />
       </mesh>
     </>
